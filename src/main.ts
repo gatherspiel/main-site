45de5f4c5a4bfd043bd9070ae22004290b7d8802
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-06-30 17:33:17 -0400

    Add some information about website and setup Vite

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Gatherspiel</h1>
+    
+    <p>Gatherspiel is a non-commercial open source platform to support in person board game events.Contact 
+    gulu@createthirdplaces.com for more details </p>
+   
+   
+    <p> If you are interested in helping with project development, go  <a href="https://github.com/gatherspiel">here</a>
+    </p>
+ 
+    
+    <p></p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
