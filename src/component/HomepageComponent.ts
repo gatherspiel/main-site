6a0e506e59212f16ae84d2855f69eab08b3deeb8
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-07-02 00:45:07 -0400

    Fix build issue

--- src/component/HomepageComponent.ts
+++ src/component/HomepageComponent.ts
@@ -5,11 +5,12 @@ export class HomepageComponent extends BasePage {
     return `
      <div>
       <h1>Gatherspiel</h1>
-      <img src="public/img/gatherspiel_logo.svg" width="400px"></img>
-      <p>Gatherspiel will a non-commercial open source platform to support in person board game events. Contact
-        gulu@createthirdplaces.com for more details </p>
+      <img src="/img/gatherspiel_logo.svg" width="400px"></img>
+      <p>Gatherspiel is a non-commercial open source platform to support in person board game events. Contact
+        gulu@createthirdplaces.com for more details. </p>
       </p>
-      <p></p>
+      
+      <p><a href="https://dmvboardgames.com/">Here</a> is an example of a site built using the Gatherspiel platform. </p>
     </div>
    `
   }
